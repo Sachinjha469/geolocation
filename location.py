d51a25049b9fc7a048f2dd63e666280de2c3e310
Ubuntu <ubuntu@ip-172-31-1-141.ap-south-1.compute.internal>
2020-05-28 13:56:37 +0000

    new flow

--- location.py
+++ location.py
@@ -3,20 +3,29 @@ from geopy.geocoders import Nominatim
 import pandas as pd
 from geopy.point import Point
 geolocator = Nominatim(timeout=5)
-with open('location123.csv') as csv_file:
+count=0
+with open('updatedlocation.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
+        count=count+1
+        print(count)
         lat=row[1]
         long=row[2]
         user_id=row[0]
         str1='"'+ lat + ',' + long + '"'
         location = geolocator.reverse(Point(lat, long))
         address=location.address
-        print(location)
+        print(location.raw)
+        try:
+            city=location.raw['address']['city']
+        except:
+            city=''
         df = pd.DataFrame({'id': [user_id],
                     'lat': [lat],
                     'long': [long],
-                    'address':[address]
+                    'address':[address],
+                    'city':[city]
+
                     })
-        df.to_csv('updatedlocation.csv',index=False,sep=',',header=None,mode='a+')
+        df.to_csv('updatedlocation12.csv',index=False,sep=',',header=None,mode='a+')
         # break
